d6960bac71edae4e53e968a04ed2657edade50d5
Davi Nakajima An <davi.nakajima.an@gmail.com>
2023-03-10 17:00:51 -0800

    Added padding parameter

--- models.py
+++ models.py
@@ -32,17 +32,17 @@ class CNN(nn.Module):
         prev_dim  = cfg.hidden_dims[0]
         k = cfg.kernel_size
         # First make the first transformation that will be passed to the hidden dims
-        layers = [nn.Conv1d(cfg.input_dim, prev_dim, kernel_size=k, padding=1), nn.ReLU(), nn.Dropout(p=cfg.p_drop)]
+        layers = [nn.Conv1d(cfg.input_dim, prev_dim, kernel_size=k, padding=cfg.padding), nn.ReLU(), nn.Dropout(p=cfg.p_drop)]
         
         # Make the hidden layers
         for next_dim in cfg.hidden_dims[1:]:
-            layers.append(nn.Conv1d(prev_dim, next_dim, kernel_size=k, padding=1))
+            layers.append(nn.Conv1d(prev_dim, next_dim, kernel_size=k, padding=cfg.padding))
             layers.append(nn.ReLU())
             layers.append(nn.Dropout(p=cfg.p_drop))
             prev_dim = next_dim
 
         # Make the last transformation
-        layers.append(nn.Conv1d(prev_dim, cfg.output_dim, kernel_size=k, padding=1))
+        layers.append(nn.Conv1d(prev_dim, cfg.output_dim, kernel_size=k, padding=cfg.padding))
 
         # define full model
         self.m = nn.Sequential(*layers)
