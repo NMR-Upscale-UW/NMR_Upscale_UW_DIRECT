e843be1f8476dbedf815d06327f9882e18a18af3
Davi Nakajima An <davi.nakajima.an@gmail.com>
2023-03-13 14:55:57 -0700

    Fixed test_models

--- test/test_models.py
+++ test/test_models.py
@@ -1,14 +1,14 @@
 import torch
 import torch.nn as nn
-from models import MLP, CNN, ConvVAE
-from model_params import DotDict, name2params
 import unittest
 import sys
 
 sys.path.append("../NMR_Upscale_UW_DIRECT")
+from models import MLP, CNN, ConvVAE
+from model_params import DotDict, name2params
 
 
-class TestMLP(nn.Module):
+class TestMLP(unittest.TestCase):
     def test_forward(self):
         cfg = name2params['mlp']
         mlp = MLP(cfg)
@@ -17,7 +17,7 @@ class TestMLP(nn.Module):
         self.assertEqual(y.shape, (32, cfg.output_dim), f"Expected output shape (32, {cfg.output_dim}), but got {y.shape}")
 
 
-class TestCNN(nn.Module):
+class TestCNN(unittest.TestCase):
     def test_forward(self):
         cfg = name2params['cnn']
         cnn = CNN(cfg)
@@ -26,14 +26,16 @@ class TestCNN(nn.Module):
         self.assertEqual(y.shape, (32, cfg.output_dim, 10), f"Expected output shape (32, {cfg.output_dim}, 10), but got {y.shape}")
 
 
-
-class TestConvVAE(nn.Module):
+class TestConvVAE(unittest.TestCase):
     def test_forward(self):
         cfg = name2params['conv_vae']
         vae = ConvVAE(cfg)
         x = torch.randn(16, cfg.encoder.input_dim, 10)
         y, mu, log_var = vae(x)
         self.assertEqual(y.shape, (16, cfg.decoder.output_dim, 10), f"Expected output shape (16, 3, 10), but got {y.shape}")
-        self.assertEqual(mu.shape , (16, cfg.encoder.output_dim), f"Expected mu shape (16, 2), but got {mu.shape}")
-        self.assertEqual(log_var.shape,(16, cfg.encoder.output_dim), f"Expected log_var shape (16, 2), but got {log_var.shape}")
+        self.assertEqual(mu.shape , (16, 10), f"Expected mu shape (16, 2), but got {mu.shape}")
+        self.assertEqual(log_var.shape,(16, 10), f"Expected log_var shape (16, 2), but got {log_var.shape}")
 
+
+if __name__ == '__main__':
+    unittest.main()
